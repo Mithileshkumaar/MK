375feb936b44de245b36a986bbe8959a9ab80a51
Mithileshkumaar <mithilesh.kumaar@yahoo.com>
2022-01-15 23:43:27 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#FE8801; boder-radius:30px" >
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href="#" style="text-decoration: underline; text-decoration-color:034454">MK CINEMAS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><router-link to="/">Home</router-link></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><router-link to="/about">About</router-link></a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#"><router-link to="/movie">Movie</router-link></a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#"><router-link to="/series">series</router-link></a>
+        </li>
+     
+        </ul>
+     
+       <input type="checkbox" style="height:30px;" >
+       <span class="check"></span>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+    </div>
+  
+</nav>
+
+    <!-- 
+     -->
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +48,45 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
+}
+
+
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+}
+
+#nav a.router-link-exact-active {
+  color: #000;
+}
+#navbar{
+  background-color:#034454;
+}
+
+input[type="checkbox"]
+{
+  -webkit-appearance: none;
+  visibility:none;
+  display: none;
+}
+.check{
+  position: relative;
+  display: block;
+  width:40px;
+  height:20px;
+  background: #034454;
+  cursor: pointer;
+  border-radius: 20px;
+  overflow: hidden;
+  transition: ease-in 0.5s;
+
+}
+input[type="checkbox"]:checked ~ .check
+{
+   background: #fff;
+   box-shadow:0 0 0 1200px #000;
+   
 }
 </style>
---
+++ src/views/movie.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="movie pt-3" style="color:white;">
+    <h1 style="color:white; text-decoration: underline; text-decoration-color:034454">MOVIES ON OUR DECK</h1>
+
+    <div class="row gy-3 pt-4" style="padding-left:40px; margin-right:30px " >
+  
+    <div class="col-6 col-sm-3" >
+     <div class="card">
+  <img src="../assets/1.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title" style="text-decoration: underline; text-decoration-color:034454">PUSHPA: THE RISE</h5>
+    <p class="card-text" >Violence erupts between red sandalwood smugglers and the police charged with bringing down their organisation in the Seshachalam forests of South India.</p>
+    <a href="https://www.youtube.com/watch?v=9MAoTL8M6hk&ab_channel=SSEntertainment" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+      <div class="card">
+  <img src="../assets/2.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title" style="text-decoration: underline; text-decoration-color:034454"> Spider-man: No Way Home</h5>
+    <p class="card-text">With Spider-Man's identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his.he finds villains from other universe </p>
+    
+    <a href="https://www.youtube.com/watch?v=ZYzbalQ6Lg8&t=70s&ab_channel=MarvelEntertainment" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+           <div class="card" >
+  <img src="../assets/3.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454"> 83 THE MOVIE</h5>
+    <p class="card-text">On June 25, 1983, the Lord's Cricket Ground witnessed 14 men beat the twice over World Champions West Indies, putting India back to cricket. with kapil leading in front</p>
+      <a href="https://www.youtube.com/watch?v=QHdkC6Kn0Io&t=98s&ab_channel=RelianceEntertainment" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+            <div class="card" >
+  <img src="../assets/4.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454; ">BEAST</h5>
+    <p class="card-text">Beast is an upcoming Indian Tamil-language black comedy action-thriller film written and directed by Nelson Dilipkumar and produced by Sun Pictures.</p>
+    <a href="https://www.youtube.com/watch?v=JO9B7aSgZOk&ab_channel=SunTV" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play Trailer</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+           <div class="card">
+  <img src="../assets/5.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black; ">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">ROCKY</h5>
+    <p class="card-text">Rocky is a 2021 Indian Tamil-language revenge drama film written and directed by debutant Arun Matheswaran.known as a good gangster film</p>
+    <a href="https://www.youtube.com/watch?v=pH1xTAnFE9M&ab_channel=LahariMusic%7CT-Series" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+           <div class="card" >
+  <img src="../assets/6.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">ANNATHE</h5>
+    <p class="card-text" style="color:white;">Kaalaiyan loves his younger sister dearly and does everything to make her happy. When she elopes with her lover to Kolkata.</p>
+    <a href="https://www.youtube.com/watch?v=VUhL0uOAkTA&ab_channel=PlipPlip" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+           <div class="card">
+  <img src="../assets/7.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">VINNAI THANDI VARUVAIYA</h5>
+    <p class="card-text">A Hindu assistant director, Karthik, falls in love with Jessie, a Christian from a traditional family A Hindu assistant director, Karthik, falls in..</p>
+    <a href="https://www.youtube.com/watch?v=9z-NeZyiyF8&ab_channel=sonymusicindia" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+           <div class="card" style="background-color:black">
+  <img src="../assets/8.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">vaarnam aayiram</h5>
+    <p class="card-text">Surya, an NSG official, is on a mission to rescue someone when he gets the news of his father's demise. He starts reminiscing .</p>
+    <a href="https://www.youtube.com/watch?v=B_TI1NV4IkY&ab_channel=RaahulNamviyar" class="btn btn-dark" >
+     <i class="fa-solid fa-play">play</i></a>
+  </div>
+</div>
+    </div>
+    
+   
+  </div>
+  </div>
+</template>
+<style scoped>
+#card-text{
+     overflow: hidden;
+
+}
+img{
+height: 300px;
+}
+
+#card-body{
+     padding-top:30px ;
+}
+
+</style>
