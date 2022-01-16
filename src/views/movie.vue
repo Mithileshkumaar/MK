00bb4b293d653075a8e75dff79613d09630cbe60
Mithileshkumaar <mithilesh.kumaar@yahoo.com>
2022-01-16 18:41:28 +0530

    hi

--- src/views/movie.vue
+++ src/views/movie.vue
@@ -5,146 +5,90 @@
     <div class="row gy-3 pt-4" style="padding-left:40px; margin-right:30px " >
   
     <div class="col-6 col-sm-3" >
-     <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/1.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-   
-    </div>
-    <div class="col-6 col-sm-3">
-      <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/2.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-   
-    </div>
-    <div class="col-6 col-sm-3">
-           <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/3.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-   
-    </div>
-    <div class="col-6 col-sm-3">
-            <div class="flip-card" >
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/4.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
+     <div class="card card-hover" style="border:none;">
+  <img src="../assets/1.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title" style="text-decoration: underline; text-decoration-color:034454">PUSHPA: THE RISE</h5>
+    <p class="card-text" >Violence erupts between red sandalwood smugglers and the police charged with bringing down their organisation in the Seshachalam forests of South India.</p>
+    <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
+     
   </div>
 </div>
     </div>
     <div class="col-6 col-sm-3">
-          <div class="flip-card" style="padding-top:300px ;">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/5.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
+      <div class="card" style="border:none;">
+  <img src="../assets/2.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title" style="text-decoration: underline; text-decoration-color:034454"> Spider-man: No Way Home</h5>
+    <p class="card-text">With Spider-Man's identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his.he finds villains from other universe </p>
+    
+     <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
   </div>
 </div>
     </div>
     <div class="col-6 col-sm-3">
-          <div class="flip-card" style="padding-top:200px ;">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/6.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
+           <div class="card" style="border:none;">
+  <img src="../assets/3.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454"> 83 THE MOVIE</h5>
+    <p class="card-text">On June 25, 1983, the Lord's Cricket Ground witnessed 14 men beat the twice over World Champions West Indies, putting India back to cricket. with kapil leading in front</p>
+       <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
   </div>
 </div>
     </div>
     <div class="col-6 col-sm-3">
-           <div class="flip-card" style="padding-top:100px ;">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/7.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
+            <div class="card" style="border:none;">
+  <img src="../assets/4.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454; ">BEAST</h5>
+    <p class="card-text">Beast is an upcoming Indian Tamil-language black comedy action-thriller film written and directed by Nelson Dilipkumar and produced by Sun Pictures.</p>
+     <div class="flip-card-back">
       <h1>John Doe</h1>
       <p>Architect  Engineer</p>
       <p>We love that guy</p>
     </div>
-  </div>
-</div>
-    </div>
-    <div class="col-6 col-sm-3 ">
-            <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/1.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-    </div>
+     <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
 
-    <div class="col-6 col-sm-3 ">
-            <div class="flip-card" style="padding-top:200px">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/1.jpg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
-    </div>
   </div>
 </div>
     </div>
-    <div class="col-6 col-sm-3 ">
-            <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="../assets/1.jpg" alt="Avatar" style="width:300px;height:300px;">
+    <div class="col-6 col-sm-3">
+           <div class="card" style="border:none;">
+  <img src="../assets/5.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black; ">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">ROCKY</h5>
+    <p class="card-text">Rocky is a 2021 Indian Tamil-language revenge drama film written and directed by debutant Arun Matheswaran.known as a good gangster film</p>
+     <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
+  </div>
+</div>
     </div>
-    <div class="flip-card-back" style="width:300px;height:300px;" >
-      <h1>John Doe</h1>
-      <p>Architect  Engineer</p>
-      <p>We love that guy</p>
+    <div class="col-6 col-sm-3">
+           <div class="card" style="border:none;">
+  <img src="../assets/6.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">ANNATHE</h5>
+    <p class="card-text" style="color:white;">Kaalaiyan loves his younger sister dearly and does everything to make her happy. When she elopes with her lover to Kolkata.</p>
+     <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
+  </div>
+</div>
     </div>
+    <div class="col-6 col-sm-3">
+           <div class="card" style="border:none;">
+  <img src="../assets/7.jpg" class="card-img-top" alt="...">
+  <div class="card-body" style="background-color:black">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">VINNAI THANDI VARUVAIYA</h5>
+    <p class="card-text">A Hindu assistant director, Karthik, falls in love with Jessie, a Christian from a traditional family A Hindu assistant director, Karthik, falls in..</p>
+     <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
+  </div>
+</div>
+    </div>
+    <div class="col-6 col-sm-3">
+           <div class="card" style="background-color:black">
+  <img src="../assets/8.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"  style="text-decoration: underline; text-decoration-color:034454">vaarnam aayiram</h5>
+    <p class="card-text">Surya, an NSG official, is on a mission to rescue someone when he gets the news of his father's demise. He starts reminiscing .</p>
+     <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
   </div>
 </div>
     </div>
@@ -181,8 +125,7 @@ height: 300px;
   background-color: transparent;
   width: 300px;
   height: 200px;
-  
- 
+  border: 1px solid #f1f1f1;
   perspective: 1000px; /* Remove this if you don't want the 3D effect */
 }
 
@@ -222,8 +165,4 @@ height: 300px;
   color: white;
   transform: rotateY(180deg);
 }
-#col-6{
- 
-}
 </style>
-// <a href="#"  > <router-link to="/push" class="btn btn-dark">play trailer</router-link></a>
